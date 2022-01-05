2e9eb8c5efbf263cb483d81e57e23160e88df0f1
Oleg Suetnow <megafox88@gmail.com>
2022-01-06 01:10:49 +0300

    remake solution for multiprojects

---
+++ VkGroupsPostSyncHelper.DAL/SQLite/VkGroupPost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace VkGroupsPostSyncHelper.DAL.SQLite
+{
+    public class VkGroupPost
+    {
+        [Key]
+        public int Id { get; set; }
+        public long? VkId { get; set; }
+        public String Text { get; set; }
+        public DateTime? PostDate { get; set; }
+        public DateTime? TransferDate { get; set; } = null;
+    }
+}
---
+++ VkGroupsPostSyncHelper.DAL/SQLite/VkPostImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace VkGroupsPostSyncHelper.DAL.SQLite
+{
+    public class VkPostImage
+    {
+        [Key]
+        public int Id { get; set; }
+        public long? VkId { get; set; }
+        public ulong Height { get; set; }
+        public ulong Width { get; set; }
+        public long PostId { get; set; }
+        public String Url { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
